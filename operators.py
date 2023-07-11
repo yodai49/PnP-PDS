d85841b89a025eb73771d487383957870c882d0b
yodai49 <cbasicjavaasembler@yahoo.co.jp>
2023-07-11 19:11:04 +0900

    Implement pds

--- operators.py
+++ operators.py
@@ -1,8 +1,14 @@
-def get_operators():
+import numpy as np
+
+def get_operators(shape, gamma1, gamma2):
     def grad_f(x):
-        return x+1
+        return x
     def prox_g(x):
-        return 2*x
+        return np.sign(x) * np.fmax(0, np.abs(x) - gamma1)
     def prox_h(x):
-        return x+3
-    return grad_f, prox_g, prox_h
+        return x
+    def phi(x):
+        return x
+    size = np.prod(shape)
+    L = np.eye(size, size)
+    return phi, grad_f, prox_g, prox_h, L
--- pds.py
+++ pds.py
@@ -1,3 +1,11 @@
-def test_pds(x_0, grad_f, prox_g, prox_h, L, max_iter):
+def test_pds(x_0, grad_f, prox_g, prox_h, L, gamma1, gamma2, max_iter):
     # 実際にPDSを解く部分
-    print(grad_f(prox_g(prox_h(0))))
+    # L is omitted to improve performance
+    x_n = x_0
+    y_n = x_0
+    for i in range(max_iter):
+        x_n1 = prox_g(x_n - gamma1 * (grad_f(x_n) + y_n))
+        y_n = prox_h(y_n - gamma2 * (2 * x_n1 - x_n))
+        x_n = x_n1
+        print('iter: ', i)
+    return x_n
