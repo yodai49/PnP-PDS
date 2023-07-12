037f7f3095fa5d0d620899891f1353afed74e42e
yodai49 <cbasicjavaasembler@yahoo.co.jp>
2023-07-12 18:05:04 +0900

    Implement gradient descent

--- operators.py
+++ operators.py
@@ -1,14 +1,45 @@
+import scipy
+import scipy.io
 import numpy as np
 
-def get_operators(shape, gamma1, gamma2):
-    def grad_f(x):
-        return x
+def get_blur_operator(x, h):
+    # return x*h where * is a convolution
+    if(np.ndim(x) == 3):
+        # color image
+        l = h.shape[0]
+        x = np.pad(x, ((0, 0), (l//2+1, l//2), (l//2+1, l//2)), 'wrap')
+        y = np.zeros(x.shape)
+        h = np.fft.fft2(h, [x.shape[-2], x.shape[-1]])
+        for i in range(x.shape[0]):
+            y[i, ...] = np.real(np.fft.ifft2(h * np.fft.fft2(x[i, ...])))
+
+    return y[..., l:, l:]
+
+def get_adj_blur_operator(x, h):
+    # return x*h where * is a convolution
+    if(np.ndim(x) == 3):
+        # color image
+        l = h.shape[0]
+        x = np.pad(x, ((0, 0), (l//2, l//2), (l//2, l//2)), 'wrap')
+        y = np.zeros(x.shape)
+        h = np.fft.fft2(h, [x.shape[-2], x.shape[-1]])
+        for i in range(x.shape[0]):
+            y[i, ...] = np.real(np.fft.ifft2(np.conj(h) * np.fft.fft2(x[i, ...])))
+
+    return y[..., :-l+1, :-l+1]
+
+def get_operators(shape, gamma1, gamma2, path_kernel):
+    def phi(x):
+        return get_blur_operator(x, h)
+    def adj_phi(x):
+        return get_adj_blur_operator(x, h)
     def prox_g(x):
         return np.sign(x) * np.fmax(0, np.abs(x) - gamma1)
     def prox_h(x):
-        return x
-    def phi(x):
-        return x
+        return np.sign(x) * np.fmax(0, np.abs(x) - gamma2)
+    
+    h = scipy.io.loadmat(path_kernel)
+    h = np.array(h['blur'])
     size = np.prod(shape)
     L = np.eye(size, size)
-    return phi, grad_f, prox_g, prox_h, L
+    return phi, adj_phi, prox_g, prox_h, L
--- test.py
+++ test.py
@@ -30,20 +30,23 @@ def eval_pds(max_iter = 1000, noise_level = 0.01, gamma1 = 1.99, gamma2 = 1.99):
     path_result = config['path_result']
 
     path_images = sorted(glob.glob(os.path.join(path_test, pattern_red)))
+    path_kernel = config['root_folder'] + '/blur_models/' + opt.kernel + '.mat'
 
     for path_img in path_images:
         img_true = cv2.imread(path_img)
         img_true = np.asarray(img_true, dtype="float32")/255.
         img_true = np.moveaxis(img_true, -1, 0)
 
-        phi, grad_f, prox_g, prox_h, L = get_operators(shape = img_true.shape, gamma1 = gamma1, gamma2 = gamma2)
+        phi, adj_phi, prox_g, prox_h, L = get_operators(shape = img_true.shape, gamma1 = gamma1, gamma2 = gamma2, path_kernel = path_kernel)
 
         noise = np.random.randn(*img_true.shape)
-        img_blur = np.copy(img_true) + noise_level * noise
+        img_blur = phi(np.copy(img_true)) + noise_level * noise
 
-        x_0 = img_blur.flatten()
+        x_0 = np.copy(img_blur)
+        def grad_f(x):
+            return adj_phi(phi(x) - x_0)
+        
         img_sol = test_pds(x_0 = x_0, grad_f = grad_f, prox_g = prox_g, prox_h = prox_h, L = L, gamma1 = gamma1, gamma2 = gamma2, max_iter = max_iter)
-        img_sol = np.reshape(img_sol, img_true.shape)
         
         pictures = [img_true, img_blur, img_sol]
         path_pictures = [path_result + 'true',  path_result + 'blur', path_result + 'sol']
