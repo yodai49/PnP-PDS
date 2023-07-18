f2d421f676276ebfd57ac67b5909c5ed1b344197
yodai49 <cbasicjavaasembler@yahoo.co.jp>
2023-07-18 14:22:13 +0900

    Implement PDS

--- pds.py
+++ pds.py
@@ -4,13 +4,12 @@ def test_pds(x_0, grad_f, prox_g, prox_h_dual, L, gamma1, gamma2, max_iter):
     x_n = x_0
     y_n = x_0
     for i in range(max_iter):
-        # for Forward-Backward algorithm
-        # x_n = prox(x_n - gamma1 * grad_f(x_n))
+        # Forward-Backward algorithm
+        #x_n = prox_g(x_n - gamma1 * grad_f(x_n))
 
-        # for Primal-dual spilitting algorithm
+        # Primal-dual spilitting algorithm
         x_prev = x_n
-        x_n = prox_g(x_n - gamma1 * grad_f(y_n))
-        #y_n = prox_h_dual(y_n + gamma2 * (2 * x_n - x_prev))
-        y_n = y_n + gamma2 * (2 * x_n - x_prev)
+        x_n = prox_g(x_n - gamma1 * (grad_f(x_n) + y_n))
+        y_n = prox_h_dual(y_n + gamma2 * (2 * x_n - x_prev))
         print('iter: ', i)
     return x_n
