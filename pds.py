ac54ac5f25cc1d6a1dae03096830e5a7b7d4bede
yodai49 <cbasicjavaasembler@yahoo.co.jp>
2023-07-25 19:58:49 +0900

    Add grid-search

--- pds.py
+++ pds.py
@@ -1,15 +1,29 @@
 import numpy as np
-def test_pds(x_0, grad_f, prox_g, prox_h_dual, L, gamma1, gamma2, max_iter):
+def psnr(img_1, img_2, data_range=1):
+    mse = np.mean((img_1.astype(float) - img_2.astype(float)) ** 2)
+    return 10 * np.log10((data_range ** 2) / mse)
+
+def test_iter(x_0, x_true, grad_f, prox_g, prox_h_dual, L, gamma1, gamma2, max_iter, method="PDS"):
     # L is omitted to improve performance
     x_n = x_0
-    y_n = x_0
+    y_n = np.zeros(x_0.shape)
+    c = np.zeros(max_iter)
     for i in range(max_iter):
-        # Forward-Backward algorithm
-        #x_n = prox_g(x_n - gamma1 * grad_f(x_n))
-
-        # Primal-dual spilitting algorithm
         x_prev = x_n
-        x_n = prox_g(x_n - gamma1 * (grad_f(x_n) + y_n))
-        y_n = prox_h_dual(y_n + gamma2 * (2 * x_n - x_prev))
-        print('iter: ', i)
-    return x_n
+
+        if(method == "FB"):
+            # Forward-Backward algorithm
+            x_n = prox_g(x_n - gamma1 * grad_f(x_n))
+        elif(method == "PDS"):
+            # Primal-dual spilitting algorithm
+            x_n = prox_g(x_n - gamma1 * (grad_f(x_n) + y_n))
+            y_n = prox_h_dual(y_n + gamma2 * (2 * x_n - x_prev))
+        else:
+            print("Unknown method:", method)
+            return x_0, c
+
+        c[i] = np.linalg.norm((x_n - x_prev).flatten()) / np.linalg.norm(x_0.flatten())
+        if(i % 50 == 0):
+            print('Method:' , method, '  iter: ', i, ' / ', max_iter)
+    
+    return x_n, c, psnr(x_n, x_true)
--- test.py
+++ test.py
@@ -3,10 +3,11 @@ import cv2
 import glob
 import json
 import numpy as np
+import matplotlib.pyplot as plt
 import os
 
 from operators import get_operators
-from pds import test_pds
+from pds import test_iter
 from utils.helpers import save_imgs
 
 parser = argparse.ArgumentParser(description="testing PnP-PDS")
@@ -24,7 +25,36 @@ opt = parser.parse_args()
 with open(opt.pth_config_file, 'r') as f:
     config = json.load(f)
 
-def eval_pds(max_iter = 1000, noise_level = 0.01, gamma1 = 1.99, gamma2 = 1.99, lambda1 = 0.1, lambda2 = 0.1):
+def grid_search(grid_num = 6):
+    param_psnr = np.zeros((grid_num, grid_num))
+    param_gamma1 = np.zeros((grid_num, grid_num))
+    param_gamma2 = np.zeros((grid_num, grid_num))
+    for i in range(0, grid_num):
+        for j in range(0, grid_num):
+            gamma1 = i / 6 + 10**-9
+            gamma2 = j / 2 + 10**-9
+            param_psnr[i][j] = 0
+            param_gamma1[i][j] = gamma1
+            param_gamma2[i][j] = gamma2
+            print(gamma1, ', ', gamma2)
+#            if (gamma1 * (0.5 + gamma2) < 1):
+            param_psnr[i][j] = eval_pds(noise_level=opt.noise_level, max_iter = opt.max_iter, gamma1 = opt.gamma1, gamma2 = opt.gamma2, lambda1 = opt.lambda1, lambda2 = opt.lambda2, result_output=False)
+    x = param_gamma1.flatten()
+    y = param_gamma2.flatten()
+    z = param_psnr.flatten()
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter3D(x, y, z, label='PSNR')
+    ax.set_title('Parameter')
+    ax.set_xlabel('gamma1')
+    ax.set_ylabel('gamma2')
+    ax.set_zlabel('psnr')
+    plt.legend()
+
+    plt.show()
+
+
+def eval_pds(max_iter = 1000, noise_level = 0.01, gamma1 = 1.99, gamma2 = 1.99, lambda1 = 0.1, lambda2 = 0.1, result_output = True):
     path_test = config['path_test']
     pattern_red = config['pattern_red']
     path_result = config['path_result']
@@ -47,11 +77,27 @@ def eval_pds(max_iter = 1000, noise_level = 0.01, gamma1 = 1.99, gamma2 = 1.99, 
         def grad_f(x):
             return adj_phi(phi(x) - x_0)
         
-        img_sol = test_pds(x_0 = x_0, grad_f = grad_f, prox_g = prox_g, prox_h_dual = prox_h_dual, L = L, gamma1 = gamma1, gamma2 = gamma2, max_iter = max_iter)
-        
-        pictures = [img_true, img_blur, img_sol]
-        path_pictures = [path_result + 'true',  path_result + 'blur', path_result + 'sol']
-        save_imgs(pictures = pictures, path_pictures = path_pictures, format = '.png')
+        img_sol, c, psnr = test_iter(x_0 = x_0, x_true = img_true, grad_f = grad_f, prox_g = prox_g, prox_h_dual = prox_h_dual, L = L, gamma1 = gamma1, gamma2 = gamma2, max_iter = max_iter, method = "FB")
+        #img_sol, c_pds, psnr_pds = test_iter(x_0 = x_0, x_true = img_true, grad_f = grad_f, prox_g = prox_g, prox_h_dual = prox_h_dual, L = L, gamma1 = gamma1, gamma2 = gamma2, max_iter = max_iter)
+            
+        if(result_output):
+            pictures = [img_true, img_blur, img_sol]
+            path_pictures = [path_result + path_img[path_img.rfind('\\'):] + '_true',  path_result +  path_img[path_img.rfind('\\'):] + '_blur', path_result + path_img[path_img.rfind('\\'):]+ '_sol']
+            save_imgs(pictures = pictures, path_pictures = path_pictures, format = '.png')
+            print(path_img)
+
+    if(result_output):
+        print('PSNR: ', psnr)
+        x = np.arange(0, max_iter, 1)
+        plt.title('Convergence')
+        plt.gca().set_yscale('log')
+        plt.plot(x, c)
+        plt.xlabel('iteration')
+        plt.ylabel('c_n')
+        plt.show()
+
+    return psnr
 
 if (__name__ == '__main__'):
-    eval_pds(noise_level=opt.noise_level, max_iter = opt.max_iter, gamma1 = opt.gamma1, gamma2 = opt.gamma2, lambda1 = opt.lambda1, lambda2 = opt.lambda2)
+    #grid_search(6)
+    eval_pds(noise_level=opt.noise_level, max_iter = opt.max_iter, gamma1 = opt.gamma1, gamma2 = opt.gamma2, lambda1 = opt.lambda1, lambda2 = opt.lambda2, result_output=True)
