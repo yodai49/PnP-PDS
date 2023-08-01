7a8e0aadc00c4de7e3d88d5cf2fc74eec1429032
82998022+yodai49@users.noreply.github.com <cbasicjavaasembler@yahoo.co.jp>
2023-08-01 11:56:20 +0900

    Add results

--- test.py
+++ test.py
@@ -6,9 +6,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-from operators import get_operators, get_blur_operators
+from operators import get_operators, get_operators_s, get_blur_operators
 from pds import test_iter
-from utils.helpers import save_imgs
+from utils.helpers import save_imgs, add_salt_and_pepper_noise
 
 parser = argparse.ArgumentParser(description="testing PnP-PDS")
 parser.add_argument("--architecture", type=str, default='DnCNN_nobn', help="architecture of network")
@@ -19,7 +19,8 @@ parser.add_argument("--lambda1", type=float, default=0.1, help='parameter of the
 parser.add_argument("--lambda2", type=float, default=0.1, help='parameter of the function h')
 parser.add_argument("--max_iter", type=int, default=1000, help='max iteration of the pds algorithm')
 parser.add_argument("--n_ch", type=int, default=3, help="channels")
-parser.add_argument("--noise_level", type=float, default=0.01, help='noise level')
+parser.add_argument("--gaussian_nl", type=float, default=0.01, help='gaussian noise level')
+parser.add_argument("--sp_nl", type=float, default=0.01, help='salt and pepper noise level')
 parser.add_argument("--pth_config_file", type=str, default='config/setup.json')
 opt = parser.parse_args()
 with open(opt.pth_config_file, 'r') as f:
@@ -30,11 +31,11 @@ def grid_search(grid_num = 6):
     param_psnr_best = np.zeros((grid_num))
     param_epsilon_dash = np.zeros((grid_num))
     for i in range(0, grid_num):
-        epsilon_dash = 0.45 + (i / grid_num)*0.2
+        epsilon_dash = 0.75 + (i / grid_num)*0.01
         param_psnr[i] = 0
         param_epsilon_dash[i] = epsilon_dash
         print('epsilon_dash: ', epsilon_dash)
-        param_psnr[i], param_psnr_best[i] = eval_pds(noise_level=opt.noise_level, max_iter = opt.max_iter, gamma1 = opt.gamma1, gamma2 = opt.gamma2, lambda1 = opt.lambda1, lambda2 = opt.lambda2, epsilon_dash = epsilon_dash, result_output=False)
+        param_psnr[i], param_psnr_best[i] = eval_pds(gaussian_nl=opt.gaussian_nl, sp_nl = opt.sp_nl, max_iter = opt.max_iter, gamma1 = opt.gamma1, gamma2 = opt.gamma2, lambda1 = opt.lambda1, lambda2 = opt.lambda2, epsilon_dash = epsilon_dash, result_output=False)
     x = param_epsilon_dash.flatten()
     y = param_psnr.flatten()
     z = param_psnr_best.flatten()
@@ -52,7 +53,7 @@ def grid_search(grid_num = 6):
     plt.show()
 
 
-def eval_pds(max_iter = 1000, noise_level = 0.01, gamma1 = 1.99, gamma2 = 1.99, lambda1 = 0.1, epsilon_dash = 0.55, lambda2 = 0.1, result_output = True):
+def eval_pds(max_iter = 1000, gaussian_nl = 0.01, sp_nl = 0.01, gamma1 = 1.99, gamma2 = 1.99, lambda1 = 0.1, epsilon_dash = 0.9, lambda2 = 0.1, result_output = True):
     path_test = config['path_test']
     pattern_red = config['pattern_red']
     path_result = config['path_result']
@@ -67,20 +68,22 @@ def eval_pds(max_iter = 1000, noise_level = 0.01, gamma1 = 1.99, gamma2 = 1.99, 
         img_true = np.moveaxis(img_true, -1, 0)
 
         phi, adj_phi = get_blur_operators(shape = img_true.shape, path_kernel = path_kernel)
-        noise = np.random.randn(*img_true.shape)
-        img_blur = phi(np.copy(img_true)) + noise_level * noise
+        gaussian_noise = np.random.randn(*img_true.shape)
+        img_blur = phi(np.copy(img_true)) + gaussian_nl * gaussian_noise
+        img_blur = add_salt_and_pepper_noise(img_blur, sp_nl)
+        
         x_0 = np.copy(img_blur)
+        eta = 0.48 * img_blur.size * sp_nl
+        grad_f, prox_g, prox_h_dual = get_operators(shape = img_true.shape, gamma1 = gamma1, gamma2 = gamma2, lambda1 = lambda1, lambda2 = lambda2, epsilon_dash=epsilon_dash, phi = phi, adj_phi = adj_phi, path_prox = path_prox, x_0 = x_0)
+        grad_f_s, prox_g_s = get_operators_s(shape = img_true.shape, eta = eta, phi = phi, x_0 = x_0)
         
-        grad_f, prox_g, prox_h_dual, L = get_operators(shape = img_true.shape, gamma1 = gamma1, gamma2 = gamma2, lambda1 = lambda1, lambda2 = lambda2, epsilon_dash=epsilon_dash, phi = phi, adj_phi = adj_phi, path_prox = path_prox, x_0 = x_0)
-        
-        img_sol, c, psnr = test_iter(x_0 = x_0, x_true = img_true, grad_f = grad_f, prox_g = prox_g, prox_h_dual = prox_h_dual, L = phi, L_T = adj_phi, gamma1 = gamma1, gamma2 = gamma2, max_iter = max_iter, method = "PDS")
-        #img_sol, c_pds, psnr_pds = test_iter(x_0 = x_0, x_true = img_true, grad_f = grad_f, prox_g = prox_g, prox_h_dual = prox_h_dual, L = L, gamma1 = gamma1, gamma2 = gamma2, max_iter = max_iter)
+        img_sol, s_sol, _, psnr = test_iter(x_0 = x_0, x_true = img_true, grad_f = grad_f, prox_g = prox_g, prox_h_dual = prox_h_dual, grad_f_s = grad_f_s, prox_g_s = prox_g_s, phi = phi, adj_phi = adj_phi, gamma1 = gamma1, gamma2 = gamma2, max_iter = max_iter, method = "PDS_S")
             
         print(path_img)
         print('PSNR: ', psnr[-1])
         if(result_output):
-            pictures = [img_true, img_blur, img_sol]
-            path_pictures = [path_result + path_img[path_img.rfind('\\'):] + '_true',  path_result +  path_img[path_img.rfind('\\'):] + '_blur', path_result + path_img[path_img.rfind('\\'):]+ '_sol']
+            pictures = [img_true, img_blur, img_sol, s_sol]
+            path_pictures = [path_result + path_img[path_img.rfind('\\'):] + '_true',  path_result +  path_img[path_img.rfind('\\'):] + '_blur', path_result + path_img[path_img.rfind('\\'):]+ '_sol', path_result + path_img[path_img.rfind('\\'):]+ '_sp_noise']
             save_imgs(pictures = pictures, path_pictures = path_pictures, format = '.png')
 
     if(result_output):
@@ -95,5 +98,5 @@ def eval_pds(max_iter = 1000, noise_level = 0.01, gamma1 = 1.99, gamma2 = 1.99, 
     return psnr[-1], np.max(psnr)
 
 if (__name__ == '__main__'):
-    #grid_search(2)
-    eval_pds(noise_level=opt.noise_level, max_iter = opt.max_iter, gamma1 = opt.gamma1, gamma2 = opt.gamma2, lambda1 = opt.lambda1, lambda2 = opt.lambda2, epsilon_dash = 0.69, result_output=True)
+    #grid_search(30)
+    eval_pds(gaussian_nl=opt.gaussian_nl, sp_nl=opt.sp_nl, max_iter = opt.max_iter, gamma1 = opt.gamma1, gamma2 = opt.gamma2, lambda1 = opt.lambda1, lambda2 = opt.lambda2, epsilon_dash = 0.9, result_output=True)
