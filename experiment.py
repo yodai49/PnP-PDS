b633971537500e985ceee48bb62f77cc3945baca
yodai49 <cbasicjavaasembler@yahoo.co.jp>
2023-08-19 20:12:03 +0900

    Run experiment

--- experiment.py
+++ experiment.py
@@ -2,5 +2,5 @@ from test import eval_restoration
 import numpy as np
 
 if (__name__ == '__main__'):
-    psnr = eval_restoration(gaussian_nl=0.02, sp_nl=0, max_iter = 300, gamma1 = 0.99, gamma2 = 0.99, r=0.8, alpha_n = 0.95, alpha_s = 0, result_output=False, architecture='preDnCNN_nobn_nch_3_nlev_0.01', deg_op = 'blur', method = 'ours-A')
+    psnr = eval_restoration(gaussian_nl=0.005, sp_nl=0, max_iter = 300, gamma1 = 0.99, gamma2 = 0.99, r=0.8, alpha_n = 0.95, alpha_s = 0, result_output=False, architecture='preDnCNN_nobn_nch_3_nlev_0.01', deg_op = 'blur', method = 'comparisonA-1')
     print(psnr, np.mean(psnr))
